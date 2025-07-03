671a76e9807cca8e5ff5036f6ba09dde40622206
Abdullah Alshalan <a.shalan443@gmail.com>
2025-07-03 13:36:58 +0300

    added ignore EF

--- C#-Fundamentals/Advanced-Concepts/obj/Debug/net9.0/Advanced-Concepts.AssemblyInfo.cs
+++ C#-Fundamentals/Advanced-Concepts/obj/Debug/net9.0/Advanced-Concepts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced-Concepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cb4806cc20cf0a71a9fe0be578e1ca62816c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c21848f98dfedb5f1b77116da508b3109babca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced-Concepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advanced-Concepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
